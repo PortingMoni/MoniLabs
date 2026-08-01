5a8f0c0572ee9cb115121de05af2257085c9be03
Jurre Groenendijk <jurre@jilles.com>
2026-08-01 12:48:48 +0000

    Port MoniRecipeTypes and related files (#16)

--- src/main/java/net/neganote/monilabs/client/gui/MoniGuiTextures.java
+++ src/main/java/net/neganote/monilabs/client/gui/MoniGuiTextures.java
@@ -1,17 +1,42 @@
 package net.neganote.monilabs.client.gui;
 
-import com.lowdragmc.lowdraglib.gui.texture.ResourceTexture;
+import com.gregtechceu.gtceu.GTCEu;
+import com.gregtechceu.gtceu.api.recipe.gui.ProgressBarTextureSet;
+
+import net.neganote.monilabs.MoniLabs;
+
+import brachy.modularui.drawable.ColorType;
+import brachy.modularui.drawable.UITexture;
 
 public class MoniGuiTextures {
 
-    public static final ResourceTexture PROGRESS_BAR_RECONSTRUCTION = new ResourceTexture(
-            "monilabs:textures/gui/progress_bar/progress_bar_reconstruction.png");
-    public static final ResourceTexture PROGRESS_BAR_ROCKET = new ResourceTexture(
-            "monilabs:textures/gui/progress_bar/progress_bar_rocket.png");
-    public static final ResourceTexture PROGRESS_BAR_SIMULATION = new ResourceTexture(
-            "monilabs:textures/gui/progress_bar/progress_bar_simulation.png");
-    public static final ResourceTexture PROGRESS_BAR_XP = new ResourceTexture(
-            "monilabs:textures/gui/progress_bar/progress_bar_xp.png");
-    public static final ResourceTexture XP_SENSOR_BUTTON = new ResourceTexture(
-            "monilabs:textures/gui/widget/button_xp_sensor.png");
+    private static UITexture progressBar(String path) {
+        UITexture.Builder builder = new UITexture.Builder()
+                .location(MoniLabs.MOD_ID, path)
+                .imageSize(40, 20)
+                .colorType(ColorType.DEFAULT);
+        return builder.build();
+    }
+
+    public static final ProgressBarTextureSet PROGRESS_BAR_RECONSTRUCTION = new ProgressBarTextureSet(
+            progressBar("textures/gui/progress_bar/progress_bar_reconstruction.png"));
+
+    public static final ProgressBarTextureSet PROGRESS_BAR_ROCKET = new ProgressBarTextureSet(
+            progressBar("textures/gui/progress_bar/progress_bar_rocket.png"));
+
+    public static final ProgressBarTextureSet PROGRESS_BAR_SIMULATION = new ProgressBarTextureSet(
+            progressBar("textures/gui/progress_bar/progress_bar_simulation.png"));
+
+    public static final ProgressBarTextureSet PROGRESS_BAR_XP = new ProgressBarTextureSet(
+            progressBar("textures/gui/progress_bar/progress_bar_xp.png"));
+
+    public static final ProgressBarTextureSet XP_SENSOR_BUTTON = new ProgressBarTextureSet(
+            progressBar("textures/gui/widget/button_xp_sensor.png"));
+
+    public static final UITexture ALWAYS_FULL_ARROW_TEXTURE = UITexture.builder()
+            .location(GTCEu.id("textures/gui/progress_bar/progress_bar_arrow.png"))
+            .imageSize(20, 40)
+            .subAreaUV(0, 0.5f, 1, 1) // We want only the second texture
+            .build();
+    public static final ProgressBarTextureSet ALWAYS_FULL_ARROW = new ProgressBarTextureSet(ALWAYS_FULL_ARROW_TEXTURE);
 }
