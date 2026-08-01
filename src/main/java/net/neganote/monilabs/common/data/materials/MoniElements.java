57bbd42deca0cada69090b81c707d7ffffe44e7a
jurrejelle <jurre@jilles.com>
2026-08-01 14:37:41 +0200

    Fix more issues and it launches now

--- src/main/java/net/neganote/monilabs/common/data/materials/MoniElements.java
+++ src/main/java/net/neganote/monilabs/common/data/materials/MoniElements.java
@@ -3,14 +3,20 @@ package net.neganote.monilabs.common.data.materials;
 import com.gregtechceu.gtceu.api.data.chemical.Element;
 import com.gregtechceu.gtceu.common.data.GTElements;
 
+import net.neganote.monilabs.MoniLabs;
+
 public class MoniElements {
 
-    public static final Element CrystalMatrix = GTElements.createAndRegister(6, 6, -1, null, "Crystal Matrix", "C☆",
+    public static final Element CrystalMatrix = GTElements.createAndRegister(MoniLabs.id("crystal_matrix"), 6, 6, -1,
+            null, "Crystal Matrix", "C☆",
             false);
-    public static final Element SculkBioalloy = GTElements.createAndRegister(-1, 481, -1, null, "Sculk Bioalloy", "ᛋ**",
+    public static final Element SculkBioalloy = GTElements.createAndRegister(MoniLabs.id("sculk_bioalloy"), -1, 481, -1,
+            null, "Sculk Bioalloy", "ᛋ**",
             false);
-    public static final Element Eltz = GTElements.createAndRegister(15, 15, -1, null, "Eltz", "Ez", false);
-    public static final Element TranscendentalMatrix = GTElements.createAndRegister(6, 32, -1, null,
+    public static final Element Eltz = GTElements.createAndRegister(MoniLabs.id("eltz"), 15, 15, -1, null, "Eltz", "Ez",
+            false);
+    public static final Element TranscendentalMatrix = GTElements.createAndRegister(
+            MoniLabs.id("transcendental_matrix"), 6, 32, -1, null,
             "Transcendental Matrix", "ᛝ",
             false);
 
--- src/main/java/net/neganote/monilabs/common/data/materials/MoniMaterialIconSets.java
+++ src/main/java/net/neganote/monilabs/common/data/materials/MoniMaterialIconSets.java
@@ -4,6 +4,8 @@ import com.gregtechceu.gtceu.api.data.chemical.material.info.MaterialIconSet;
 
 public class MoniMaterialIconSets {
 
+    public static void init() {}
+
     public static MaterialIconSet Crystal = new MaterialIconSet("crystal", MaterialIconSet.SHINY);
     public static MaterialIconSet SculkAlloy = new MaterialIconSet("sculk_alloy", MaterialIconSet.DULL);
     public static MaterialIconSet Eltz = new MaterialIconSet("eltz", MaterialIconSet.SHINY);
